b7ea3919d2e82f9052adf7700d9b508d55124c12
Leone Daher <lcsouza@mag.com.br>
2021-02-11 12:38:31 -0300

    <init>Initial Commit
    add higher level of abstraction @main.py with the basic steps

---
+++ main.py
@@ -0,0 +1,20 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Press the green button in the gutter to run the script.
+def screening_presentation():
+    pass
+
+def screening_form():
+    pass
+
+def screening_send_result():
+    pass
+
+
+if __name__ == '__main__':
+    screening_presentation()
+    screening_form()
+    screening_send_result()
